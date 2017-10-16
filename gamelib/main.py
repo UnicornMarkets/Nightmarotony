8435dabf1c84560d8cbda1098cb39c9c1c5934c1
xsun40 <xsun40@hawk.iit.edu>
2017-10-16 14:54:11 -0500

    build book-shelf and shelf event

--- gamelib/main.py
+++ gamelib/main.py
@@ -16,15 +16,33 @@ def main():
     keys = {'w': False, 's': False, 'a': False, 'd': False}
     playerpos = [100, 100]
     screen = pygame.display.set_mode((width, height))
+    grass = pygame.image.load("resources/images/grass.png")
     player = pygame.image.load("resources/images/person.png")
+    shelf = pygame.image.load("resources/images/shelf.png")
+    shelf2 = pygame.image.load("resources/images/222.jpg")
     while 1:
         screen.fill(0)
+        for x in range(0, width // grass.get_width() + 1):
+            for y in range(0, height // grass.get_height() + 1):
+                screen.blit(grass, (x * 100, y * 100))
         screen.blit(player, playerpos)
+        screen.blit(shelf, (0, 30))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit(0)
+            if playerpos[0] >=-5 and playerpos[0] <=0 and playerpos[1] >=25 and playerpos[1] <=35 \
+                                and event.type==pygame.MOUSEBUTTONDOWN:
+                screen1 = pygame.display.set_mode((width, height))
+                close = 1
+                while close:
+                    screen1.fill(0)
+                    screen1.blit(shelf2, [0,30])
+                    pygame.display.flip()
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            close = 0
             if event.type == pygame.KEYDOWN:
                 if event.key == K_w:
                     keys['w'] = True
