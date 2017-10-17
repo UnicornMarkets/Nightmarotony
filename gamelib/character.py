842a192c165002c1ebf9c3c8363a27a8a6254ae4
xsun40 <xsun40@hawk.iit.edu>
2017-10-17 13:54:35 -0500

    build shelf class

--- gamelib/character.py
+++ gamelib/character.py
@@ -5,7 +5,8 @@ class Character(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super(Character, self).__init__(*groups)
         self.sprite_dict = {}
-        self.speed = 15
+        self.speed = 35
+        self.block_direction = []
         self.direction = {0:'front',1:'front_right', 2:'right',3:'right_back',
                           4: 'back', 5:'back_left', 6:'left', 7:'left_front'}
         self.now_direction = 0
@@ -24,7 +25,8 @@ class Character(pygame.sprite.Sprite):
         if key[pygame.K_RIGHT] or key[pygame.K_d]:
             self.change_direction(1)
         if key[pygame.K_UP] or key[pygame.K_w]:
-            self.move()
+            if self.now_direction not in self.block_direction:
+                self.move(game.shelf.rect)
         if key[pygame.K_DOWN] or key[pygame.K_s]:
             self.change_direction(4)
         self.image = self.sprite_dict[self.direction[self.now_direction]]
@@ -36,26 +38,49 @@ class Character(pygame.sprite.Sprite):
         elif self.now_direction > 7:
             self.now_direction -= 8
 
-    def move(self):
+    def move(self, shelf_rect):
         if self.now_direction == 0:
             self.rect.y -= self.speed
+            self.check_block(shelf_rect, 0)
         if self.now_direction == 1:
             self.rect.y -= sqrt((self.speed ^ 2) / 2)
             self.rect.x += sqrt((self.speed ^ 2) / 2)
+            self.check_block(shelf_rect, 1)
         if self.now_direction == 2:
             self.rect.x += self.speed
+            self.check_block(shelf_rect, 2)
         if self.now_direction == 3:
             self.rect.y += sqrt((self.speed ^ 2) / 2)
             self.rect.x += sqrt((self.speed ^ 2) / 2)
+            self.check_block(shelf_rect, 3)
         if self.now_direction == 4:
             self.rect.y += self.speed
+            self.check_block(shelf_rect, 4)
         if self.now_direction == 5:
             self.rect.y += sqrt((self.speed ^ 2) / 2)
             self.rect.x -= sqrt((self.speed ^ 2) / 2)
+            self.check_block(shelf_rect, 5)
         if self.now_direction == 6:
             self.rect.x -= self.speed
+            self.check_block(shelf_rect, 6)
         if self.now_direction == 7:
             self.rect.y -= sqrt((self.speed ^ 2) / 2)
             self.rect.x -= sqrt((self.speed ^ 2) / 2)
+            self.check_block(shelf_rect, 7)
+
+    def check_block(self, shelf_rect, direction):
+        if self.rect.colliderect(shelf_rect):
+
+            self.block_direction += [direction]
+            if direction - 1 < 0:
+                self.block_direction += [direction + 7]
+            else:
+                self.block_direction += [direction - 1]
+            if direction + 1 > 7:
+                self.block_direction += [direction - 7]
+            else:
+                self.block_direction += [direction + 1]
+        else:
+            self.block_direction = []
 
 
